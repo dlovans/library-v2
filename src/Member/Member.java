57824f8369a53b2a59e9d9989d5e3ee912a89eaf
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-26 20:33:09 +0200

    Declare Member class

---
+++ src/Member/Member.java
@@ -0,0 +1,51 @@
+package Member;
+import Media.Media;
+import java.util.ArrayList;
+
+/**
+ * Represents a library member.
+ */
+public class Member {
+    /**
+     * Loaned media.
+     */
+    protected ArrayList<Media> loanedMedia = new ArrayList<Media>();
+
+    /**
+     * If member is a VIP.
+     */
+    protected boolean isVIP;
+
+    /**
+     * Creates an instance of Member with default values.
+     */
+    public Member() {
+        this.isVIP = false;
+    }
+
+    /**
+     * Upgrades a member to VIP status.
+     * @return - Whether member has been upgraded or not.
+     */
+    protected boolean upgradeMember() {
+        if (this.isVIP) {
+            return false;
+        } else {
+            this.isVIP = true;
+            return true;
+        }
+    }
+
+    /**
+     * Downgrades a member to regular status.
+     * @return - Whether member has been downgraded or not.
+     */
+    protected boolean downgradeMember() {
+        if (this.isVIP) {
+            this.isVIP = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
