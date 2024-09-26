6c6fd98030a1d85ef21bee511de6e0030d576401
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-26 20:14:24 +0200

    Update constructor

--- src/Media/AuthoredMedia.java
+++ src/Media/AuthoredMedia.java
@@ -1,15 +1,22 @@
 package Media;
 
 /**
- * Represents authored media.
+ * Represents authored media, subclassing Media.
  */
-public abstract class AuthoredMedia {
+public abstract class AuthoredMedia extends Media {
     /**
      * Author of written media.
      */
     protected String author;
 
-    public AuthoredMedia(String author) {
+    /**
+     * Creates an instance of AuthoredMedia.
+     * @param title - Title of authored media.
+     * @param author - Author of authored media.
+     * @param publishingYear - Authored media publishing year.
+     */
+    public AuthoredMedia(String title, String author, int publishingYear) {
+        super(title, publishingYear);
         this.author = author;
     }
 }
