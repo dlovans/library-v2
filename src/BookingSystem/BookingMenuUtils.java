cd67846cfb7eef443c37feeaeeb563a13dfbdaa5
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-27 13:10:29 +0200

    Add import statements

--- src/BookingSystem/BookingMenuUtils.java
+++ src/BookingSystem/BookingMenuUtils.java
@@ -1,7 +1,16 @@
 package BookingSystem;
 
+import Member.*;
+import Media.*;
+
+import java.util.Scanner;
+
 /**
- * Utility class for media submenu.
+ * Utility class for booking submenu.
  */
 public class BookingMenuUtils {
+    /**
+     * Scanner object to read console stream.
+     */
+    final private Scanner scanner = new Scanner(System.in);
 }
