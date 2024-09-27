cc731234e0d199df0737765ee679966cfac14bfb
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-27 12:43:53 +0200

    Define method to create a member

--- src/BookingSystem/MemberMenuUtils.java
+++ src/BookingSystem/MemberMenuUtils.java
@@ -1,7 +1,32 @@
 package BookingSystem;
 
+import Member.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Utility class for member submenu.
  */
 public class MemberMenuUtils {
+    /**
+     * Scanner object to read console stream.
+     */
+    final private Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Creates a member, appends member to collection.
+     * @param members - Collection of library members.
+     */
+    protected void createMember(ArrayList<Member> members) {
+        String name;
+        int birthYear;
+
+        System.out.println("Enter the full name of the new member: ");
+        name = scanner.nextLine();
+        System.out.println("Enter the birth year of the new member: ");
+        birthYear = scanner.nextInt();
+        members.add(new Member(name, birthYear));
+        System.out.println("Members added successfully.");
+        System.out.println();
+    }
 }
