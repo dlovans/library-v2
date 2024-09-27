aaa208199edce5e199208e618d80604af0b14c51
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-27 14:42:36 +0200

    Declare method that returns lent media and refactor methods

--- src/BookingSystem/BookingMenuUtils.java
+++ src/BookingSystem/BookingMenuUtils.java
@@ -43,11 +43,13 @@ public class BookingMenuUtils extends MenuUtils {
         if (indexOfMediaToLoan == -1) {
             System.out.println("Library has no media.");
             System.out.println("Returning to main menu");
+            return;
         }
 
         media = medias.get(indexOfMediaToLoan);
         if (media.loan(member.getMemberStatus())) {
             member.addLentMedia(media);
+            medias.remove(indexOfMediaToLoan);
             System.out.println("Successfully loaned member.");
         } else {
             System.out.println("Failed to loan member.");
@@ -63,7 +65,7 @@ public class BookingMenuUtils extends MenuUtils {
     private int listAndSelectMedia(ArrayList<Media> medias) {
         int indexOfMedia;
 
-        if (medias.isEmpty()) { return -1; };
+        if (medias.isEmpty()) { return -1; }
 
         for (int i = 0; i < medias.size(); i++) {
             System.out.println((i + 1) + ". " + medias.get(i));
@@ -72,5 +74,45 @@ public class BookingMenuUtils extends MenuUtils {
         indexOfMedia = scanner.nextInt() - 1;
         scanner.nextLine();
         return indexOfMedia;
+    }
+
+    protected void returnBooking(ArrayList<Member> members, ArrayList<Media> medias) {
+        int indexOfMarkedMember;
+        int indexOfMediaToReturn;
+        Member member;
+        System.out.println("Select member making a return? ");
+
+        indexOfMarkedMember = super.listAndSelectMember(members);
+
+        if (indexOfMarkedMember == -1) {
+            System.out.println("Library has no members.");
+            System.out.println("Returning to main menu");
+            return;
+        }
+
+        member = members.get(indexOfMarkedMember);
+
+        System.out.println("Select media to return: ");
+        int counter = 0;
+        ArrayList<Media> lentMedias = member.getLoanedMedia();
+        for (Media lentMedia: lentMedias) {
+            System.out.println((counter + 1) + ". " + lentMedia);
+            counter++;
+        }
+
+        indexOfMediaToReturn = scanner.nextInt() - 1;
+        scanner.nextLine();
+
+        if (indexOfMediaToReturn == -1) {
+            System.out.println("Library has no media.");
+            System.out.println("Returning to main menu");
+            return;
+        }
+
+        medias.add(lentMedias.get(indexOfMediaToReturn));
+        lentMedias.remove(indexOfMediaToReturn);
+        System.out.println("Media successfully returned.");
+        System.out.println("Returning to main menu");
+        System.out.println();
     }
 }
