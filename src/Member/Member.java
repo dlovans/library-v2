8e7c0f24bb34c2940ffb1ecb77f9d8856b3ecb04
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-27 12:43:40 +0200

    Add field birthYear

--- src/Member/Member.java
+++ src/Member/Member.java
@@ -10,7 +10,7 @@ public class Member {
     /**
      * Loaned media.
      */
-    protected ArrayList<Media> loanedMedia = new ArrayList<Media>();
+    final protected ArrayList<Media> loanedMedia = new ArrayList<Media>();
 
     /**
      * If member is a VIP.
@@ -20,14 +20,17 @@ public class Member {
     /**
      * Member full name.
      */
-    protected String fullName;
+    final protected String fullName;
+
+    final protected int birthYear;
 
     /**
      * Creates an instance of Member with default and specified values.
      */
-    public Member(String fullName) {
-        this.isVIP = false;
+    public Member(String fullName, int birthYear) {
         this.fullName = fullName;
+        this.birthYear = birthYear;
+        this.isVIP = false;
     }
 
     /**
